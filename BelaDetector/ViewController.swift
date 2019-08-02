753934cf1aacfae9e8395acbcfaaa98bbdbb14c0
Dominik Cubelic <dominik.cubelic@me.com>
2019-08-03 00:34:46 +0200

    Initial Commit

---
+++ BelaDetector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BelaDetector
+//
+//  Created by Dominik Cubelic on 03/08/2019.
+//  Copyright © 2019 Dominik Cubelic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
