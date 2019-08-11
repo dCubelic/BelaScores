c932186b824a8a594ebb943f918af0a2502bca18
Dominik Cubelic <dominik.cubelic@me.com>
2019-08-11 21:53:56 +0200

    score cell bidding team dot view

--- BlokZaBelu/Score/Cells/ScoreTableViewCell.swift
+++ BlokZaBelu/Score/Cells/ScoreTableViewCell.swift
@@ -12,7 +12,8 @@ class ScoreTableViewCell: UITableViewCell {
 
     @IBOutlet weak private var score1Label: UILabel!
     @IBOutlet weak private var score2Label: UILabel!
-    @IBOutlet weak private var trumpSuitImageView: UIImageView!
+    @IBOutlet weak var team1BidView: UIView!
+    @IBOutlet weak var team2BidView: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -22,6 +23,9 @@ class ScoreTableViewCell: UITableViewCell {
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
+        
+        team1BidView.layer.cornerRadius = team1BidView.frame.height / 2
+        team2BidView.layer.cornerRadius = team2BidView.frame.height / 2
         
         contentView.backgroundColor = selected ? UIColor.darkGray2 : UIColor.darkGray
     }
@@ -35,7 +39,8 @@ class ScoreTableViewCell: UITableViewCell {
     func setup(for score: BelaScore) {
         score1Label.text = String(score.score1)
         score2Label.text = String(score.score2)
-        trumpSuitImageView.image = UIImage(named: score.trumpSuit.imageName)
+        team1BidView.isHidden = false
+        team2BidView.isHidden = true
     }
     
 }
